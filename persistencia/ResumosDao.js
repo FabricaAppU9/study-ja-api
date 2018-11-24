9401caadc6fb8e4ebe342cb69c63e776127a31a8
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2018-11-24 19:20:00 -0200

    removing nonexistent column

--- persistencia/ResumosDao.js
+++ persistencia/ResumosDao.js
@@ -13,7 +13,7 @@ Resumos.prototype.salvaResumoLivroTransaction = function (resumo, callback) {
                 });
             }
             const lastId = result.insertId;
-            this._connection.query('insert into livro (tra_id,liv_nome,liv_editora,liv_volume,liv_ano) VALUES(?,?,?,?);', lastId, resumo[7], resumo[8], resumo[9], function (error, result) {
+            this._connection.query('insert into livro (tra_id,liv_nome,liv_editora,liv_ano) VALUES(?,?,?,?);', lastId, resumo[7], resumo[8], resumo[9], function (error, result) {
                 if (error) {
                     return this._connection.rollback(function () {
                         callback(error, null);
@@ -32,7 +32,7 @@ Resumos.prototype.salvaResumoLivroTransaction = function (resumo, callback) {
     });
 }
 Resumos.prototype.salvaResumoLivro = function (resumo, callback) {
-    this._connection.query(`START TRANSACTION; insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_texto,tra_dt_criacao,tra_visualizacoes) VALUES (?,?,?,?,now(),?); insert into livro (tra_id,liv_nome,liv_editora,liv_volume,liv_ano) VALUES(LAST_INSERT_ID(),?,?,?); COMMIT;`, [resumo[0], resumo[1], resumo[2], resumo[3], resumo[5], resumo[6], resumo[7], resumo[8], resumo[9]], callback);
+    this._connection.query(`START TRANSACTION; insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_texto,tra_dt_criacao,tra_visualizacoes) VALUES (?,?,?,?,now(),?); insert into livro (tra_id,liv_nome,liv_editora,liv_ano) VALUES(LAST_INSERT_ID(),?,?,?); COMMIT;`, [resumo[0], resumo[1], resumo[2], resumo[3], resumo[5], resumo[6], resumo[7], resumo[8], resumo[9]], callback);
 }
 Resumos.prototype.salvaResumoArtigo = function (resumo, callback) {
     this._connection.query(`START TRANSACTION; insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_texto,tra_dt_criacao,tra_visualizacoes) VALUES (?,?,?,?,now(),?); insert into artigo(tra_id,art_titulo) VALUES (LAST_INSERT_ID(),?); COMMIT;`, [resumo[0], resumo[1], resumo[2], resumo[3], resumo[5], resumo[6], resumo[7]], callback);
