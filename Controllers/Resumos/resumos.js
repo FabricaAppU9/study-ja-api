5b8861ae094d59fa13c6ca6e0b51493af6f5d100
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2018-11-23 23:06:24 -0200

    adjust in querie to post new resume from book

--- Controllers/Resumos/resumos.js
+++ Controllers/Resumos/resumos.js
@@ -1,64 +1,67 @@
 const TokenAuth = require('../../Middlewares/Token-auth');
 const AdminAuth = require('../../Middlewares/AdminToken-auth');
-module.exports=function(app){
-    app.get('/resumos/livros',(req,res) => {
-     var connection = app.persistencia.connectionFactory();
-     connection.connect();
-     var resumosDao = new app.persistencia.ResumosDao(connection);
-      resumosDao.listaResumosLivro((err,resultado) => {
-        if (!err){
-            console.log("aqui estou eu");
-            res.json(resultado);
-        }
-        else
-            console.log('Error while performing Query.' + err);
-      });        
-      connection.end();
-    });
-    app.get('/resumos/artigos',(req,res) => {
+module.exports = function (app) {
+    app.get('/resumos/livros', (req, res) => {
         var connection = app.persistencia.connectionFactory();
         connection.connect();
         var resumosDao = new app.persistencia.ResumosDao(connection);
-         resumosDao.listaResumosArtigo((err,resultado) => {
-           if (!err){
-               console.log("aqui estou eu");
-               res.json(resultado);
-           }
-           else
-               console.log('Error while performing Query.' + err);
-         });        
-         connection.end();
-       });
-    
-    app.post('/resumos/resumo',TokenAuth,(req, res) => {
+        resumosDao.listaResumosLivro((err, resultado) => {
+            if (!err) {
+                console.log("aqui estou eu");
+                res.json(resultado);
+            }
+            else
+                console.log('Error while performing Query.' + err);
+        });
+        connection.end();
+    });
+    app.get('/resumos/artigos', (req, res) => {
+        var connection = app.persistencia.connectionFactory();
+        connection.connect();
+        var resumosDao = new app.persistencia.ResumosDao(connection);
+        resumosDao.listaResumosArtigo((err, resultado) => {
+            if (!err) {
+                console.log("aqui estou eu");
+                res.json(resultado);
+            }
+            else
+                console.log('Error while performing Query.' + err);
+        });
+        connection.end();
+    });
+
+    app.post('/resumos/resumo', TokenAuth, (req, res) => {
         const resumo = req.body;
-        var validatorTitulo = req.assert('titulo', 'Titulo é obrigatório').notEmpty();
+        // var validatorTitulo = req.assert('titulo', 'Titulo é obrigatório').notEmpty();
         var erros = req.validationErrors();
-        if(erros){
-            return res.status(422).json(erros);    
-           
-            
+        if (erros) {
+            return res.status(422).json(erros);
         }
         //lembrando que os nomes dos campos no BANCO DE DADOS, devem ter o mesmo nome da tag NAME, nos formularios
         // HTML;
         var connection = app.persistencia.connectionFactory();
         var resumosDao = new app.persistencia.ResumosDao(connection);
 
-        resumosDao.salvaResumoLivro(resumo,function(error,resultado){
-           if(!error){
-            console.log('resumo criado');
-            res.status(200).json(resultado);  
-           } 
-           else
+        resumosDao.salvaResumoLivro(resumo, function (error, resultado) {
+            if (error) {
+                res.status(400).json({
+                    error: error
+                })
+            }
+            if (!error) {
+                console.log('resumo criado');
+                res.status(200).json(resultado);
+            }
+            else
                 console.log("error performing POST" + error);
         });
         connection.end();
     });
-    app.get('/resumos/resumo/:id',(req,res) => {
+    app.get('/resumos/resumo/:id', (req, res) => {
         const id = req.params.id;
         let validatorId = req.assert('id', 'id é obrigatório').notEmpty();
         let erros = req.validationErrors();
-        if (erros){
+        if (erros) {
             res.json(erros);
             res.status(422);
             return;
@@ -66,132 +69,132 @@ module.exports=function(app){
         var connection = app.persistencia.connectionFactory();
         var resumosDao = new app.persistencia.ResumosDao(connection);
 
-        resumosDao.listaId(id,(error,resultado)=>{
-            if(JSON.stringify(resultado)==='[]'){
+        resumosDao.listaId(id, (error, resultado) => {
+            if (JSON.stringify(resultado) === '[]') {
                 res.status(200);
                 res.json({
-                    mensagem : 'Resumo não existente'
+                    mensagem: 'Resumo não existente'
                 });
                 return;
             }
-            if(!error){
+            if (!error) {
                 res.send(resultado);
                 res.status(200);
             }
-            else{
+            else {
                 res.status(500);
                 res.send(error);
             }
         });
         connection.end();
     });
-    
-    app.get('/resumos/categorias',(req,res,next)=>{
+
+    app.get('/resumos/categorias', (req, res, next) => {
         let connection = app.persistencia.connectionFactory();
         var resumosDao = new app.persistencia.ResumosDao(connection);
-        resumosDao.listaCategorias((err,resultado)=>{
-            if(!err){
+        resumosDao.listaCategorias((err, resultado) => {
+            if (!err) {
                 res.status(200).json({
-                    resultado:resultado
+                    resultado: resultado
                 })
-            }else{
+            } else {
                 res.status(400).json({
-                    error:err
+                    error: err
                 })
             }
         });
     });
-    app.get('/resumos/categoria/:id',(req,res,next)=>{
+    app.get('/resumos/categoria/:id', (req, res, next) => {
         let id = req.params.id
         let connection = app.persistencia.connectionFactory();
         var resumosDao = new app.persistencia.ResumosDao(connection);
-        resumosDao.resumosLivroCategoria(id,(err,resultado)=>{
-            if(!err){
+        resumosDao.resumosLivroCategoria(id, (err, resultado) => {
+            if (!err) {
                 res.status(200).json({
                     Resumos: resultado
                 });
             }
         });
     });
-    app.post('/resumos/resumo/avaliar',TokenAuth,(req,res,next)=>{
+    app.post('/resumos/resumo/avaliar', TokenAuth, (req, res, next) => {
         let info = req.body;
         let connection = app.persistencia.connectionFactory();
         var resumosDao = new app.persistencia.ResumosDao(connection);
-        resumosDao.resumosAvaliacao(info,(err,resultado)=>{
-            if(!err){
+        resumosDao.resumosAvaliacao(info, (err, resultado) => {
+            if (!err) {
                 res.status(200).json({
                     Mensagem: "Resumo Avaliado com sucesso!"
                 });
-            }else{
+            } else {
                 res.status(400).json({
-                    Mensagem:"Não foi possível avaliar este resumo",
-                    error:err
+                    Mensagem: "Não foi possível avaliar este resumo",
+                    error: err
                 });
             }
         });
     });
-    app.get('/resumos/resumo/avaliado/:id',(req,res,next)=>{
+    app.get('/resumos/resumo/avaliado/:id', (req, res, next) => {
         let id = req.params.id;
-        let connection= app.persistencia.connectionFactory();
-        let resumosDao= new app.persistencia.ResumosDao(connection);
+        let connection = app.persistencia.connectionFactory();
+        let resumosDao = new app.persistencia.ResumosDao(connection);
 
-        resumosDao.getAvaliacao(id,(err,resultado)=>{
-            if(!err){
-                let avaliacoes=0;
+        resumosDao.getAvaliacao(id, (err, resultado) => {
+            if (!err) {
+                let avaliacoes = 0;
                 console.log(resultado.length);
-                for(let i=0;i<resultado.length;i++){
-                   let numeros=(parseInt(resultado[i].est_valor));
-                   avaliacoes+=numeros;
-                   console.log(avaliacoes);
+                for (let i = 0; i < resultado.length; i++) {
+                    let numeros = (parseInt(resultado[i].est_valor));
+                    avaliacoes += numeros;
+                    console.log(avaliacoes);
                 }
-                let mediaAvaliacoes=avaliacoes/resultado.length;
+                let mediaAvaliacoes = avaliacoes / resultado.length;
                 res.status(200).json({
-                    resultado:mediaAvaliacoes
+                    resultado: mediaAvaliacoes
                 });
-            }else{
+            } else {
                 res.status(400).json({
-                    mensagem:"Não foi possível avaliar este resumo",
-                    error:err
+                    mensagem: "Não foi possível avaliar este resumo",
+                    error: err
                 })
             }
         });
     });
-    app.post('/resumos/resumo/comentar',TokenAuth,(req,res,next)=>{
-        let tra_id= req.body.comnt_tra_id;
-        let usu_id= req.body.comnt_usu_id;
-        let coment= req.body.comnt_comentario;
+    app.post('/resumos/resumo/comentar', TokenAuth, (req, res, next) => {
+        let tra_id = req.body.comnt_tra_id;
+        let usu_id = req.body.comnt_usu_id;
+        let coment = req.body.comnt_comentario;
 
-        let connection= app.persistencia.connectionFactory();
+        let connection = app.persistencia.connectionFactory();
         connection.connect();
-        let resumosDao= new app.persistencia.ResumosDao(connection);
-        resumosDao.newComent(usu_id,tra_id,coment,(err,resultado)=>{
-            if(!err){
+        let resumosDao = new app.persistencia.ResumosDao(connection);
+        resumosDao.newComent(usu_id, tra_id, coment, (err, resultado) => {
+            if (!err) {
                 res.status(200).json({
-                    Mensagem:"Comentário feito com sucesso!",
-                    resultado:resultado
+                    Mensagem: "Comentário feito com sucesso!",
+                    resultado: resultado
                 });
-            }else{
+            } else {
                 res.status(400).json({
                     Mensagem: "Não possível efetuar o comentário",
-                    error:err
+                    error: err
                 });
             }
         });
     });
-    app.get('/resumos/resumo/comentarios/:id',(req,res,nexte)=>{
+    app.get('/resumos/resumo/comentarios/:id', (req, res, nexte) => {
         let trab_id = req.params.id;
 
         let connect = app.persistencia.connectionFactory();
         let resumosDao = new app.persistencia.ResumosDao(connect);
-        resumosDao.getComent(trab_id,(err,resultado)=>{
-            if(!err){
+        resumosDao.getComent(trab_id, (err, resultado) => {
+            if (!err) {
                 res.status(200).json({
-                    comentarios:resultado
+                    comentarios: resultado
                 });
-            }else{
+            } else {
                 res.status(400).json({
                     Mensagem: "Não foi possível carregar os comentários",
-                    error:err
+                    error: err
                 });
             }
         });
--- persistencia/ResumosDao.js
+++ persistencia/ResumosDao.js
@@ -1,59 +1,59 @@
- function Resumos(connection){
-        this._connection= connection;
-    }
-    
-        Resumos.prototype.salvaResumoLivro = function (resumo, callback) {
-            this._connection.query("START TRANSACTION;"+
-            "insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_tipo,tra_texto,tra_dt_criacao,tra_dt_atualizacao,tra_visualizacoes)"+
-           "VALUES (1,2,testes,texto teste para inser com transaction, now(),now(),12);"+
-           "insert into livro (liv_nome,liv_editora,liv_volume,liv_ano)"+
-            "VALUES(LAST_INSERT_ID(),TESTE,testeeditora,volume 2,liv_ano);"+
-            "COMMIT;",resumo,callback);
-        }
-        Resumos.prototype.salvaResumoArtigo= function(resumo,callback){
-            this._connection.query("START TRANSACTION;"+
-                "insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_tipo,tra_texto,tra_dt_criacao,tra_dt_atualizacao,tra_visualizacoes)"+
-                "VALUES (1,2,testes,texto teste para inser com transaction, now(),now(),12);"+
-                "insert into artigo(tra_id,art_titulo)"+
-                "VALUES (LAST_INSERT_ID(),teste dois);"+
-                "COMMIT;",resumo,callback);
-        }
-        Resumos.prototype.listaResumosLivro= function(callback){
-            this._connection.query('select t.*,l.*,u.usu_nome from trabalho t inner join livro l on t.tra_id = l.tra_id inner join usuario u on t.tra_usu_id=u.usu_id', callback);
-        }
-        Resumos.prototype.listaResumosArtigo= function(callback){
-            this._connection.query('select t.*,a.*,u.usu_nome from trabalho t inner join artigo a on t.tra_id = a.tra_id inner join usuario u on t.tra_usu_id=u.usu_id', callback);
-        }
-        Resumos.prototype.listaId= function(id,callback){
-            this._connection.query('select * from trabalho where tra_id = ?',id,callback);
-        }
-        Resumos.prototype.editar= function(id,callback){
-            this._connection.query("update trabalho set trab_inf_post = 1 where usu_id = ? ",id, callback);
-        }
-        Resumos.prototype.listaResumosPendentes = function(callback){
-            this._connection.query('select * from trabalho where tra_verifica = 0',callback);
-        };
-        Resumos.prototype.listaCategorias=function(callback){
-            this._connection.query('select * from categoria',callback);
-        }
-        Resumos.prototype.resumosLivroCategoria=function(id,callback){
-            this._connection.query('select t.*, l.* from trabalho t inner join livro l on t.tra_id=l.tra_id where tra_cat_id = ?',id,callback);
-        }
-        Resumos.prototype.resumosAvaliacao=function(info,callback){
-            this._connection.query('insert into estrela set ?',info,callback);
-        }
-        Resumos.prototype.getAvaliacao=function(id,callback){
-            this._connection.query('select e.est_valor from estrela e where est_tra_id = ?',id,callback);
-        }
-        Resumos.prototype.newComent=function(usu_id,tra_id,comentario,callback){
-            this._connection.query('insert into comentario (comnt_usu_id,comnt_tra_id,comnt_comentario) VALUES (?,?,?)',[usu_id,tra_id,comentario],callback);
-        }
-        Resumos.prototype.getComent=function(comnt_trab_id,callback){
-            this._connection.query('select c.*,u.usu_nome,u.usu_sobrenome,u.usu_img_perfil from comentario c inner join trabalho t on c.comnt_tra_id=t.tra_id inner join usuario u on c.comnt_usu_id=u.usu_id where comnt_tra_id = ?',comnt_trab_id,callback);
-        }
-        
+function Resumos(connection) {
+    this._connection = connection;
+}
 
-    module.exports = function(){
-        return Resumos;
-    }
+Resumos.prototype.salvaResumoLivro = function (resumo, callback) {
+    this._connection.query("START TRANSACTION;" +
+        "insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_texto,tra_dt_criacao,tra_dt_atualizacao,tra_visualizacoes)" +
+        "VALUES (1,2,testes,texto teste para inser com transaction, now(),now(),12);" +
+        "insert into livro (tra_id,liv_nome,liv_editora,liv_volume,liv_ano)" +
+        "VALUES(LAST_INSERT_ID(),TESTE,testeeditora,volume 2,liv_ano);" +
+        "COMMIT;", resumo, callback);
+}
+Resumos.prototype.salvaResumoArtigo = function (resumo, callback) {
+    this._connection.query("START TRANSACTION;" +
+        "insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_tipo,tra_texto,tra_dt_criacao,tra_dt_atualizacao,tra_visualizacoes)" +
+        "VALUES (1,2,testes,texto teste para inser com transaction, now(),now(),12);" +
+        "insert into artigo(tra_id,art_titulo)" +
+        "VALUES (LAST_INSERT_ID(),teste dois);" +
+        "COMMIT;", resumo, callback);
+}
+Resumos.prototype.listaResumosLivro = function (callback) {
+    this._connection.query('select t.*,l.*,u.usu_nome from trabalho t inner join livro l on t.tra_id = l.tra_id inner join usuario u on t.tra_usu_id=u.usu_id', callback);
+}
+Resumos.prototype.listaResumosArtigo = function (callback) {
+    this._connection.query('select t.*,a.*,u.usu_nome from trabalho t inner join artigo a on t.tra_id = a.tra_id inner join usuario u on t.tra_usu_id=u.usu_id', callback);
+}
+Resumos.prototype.listaId = function (id, callback) {
+    this._connection.query('select * from trabalho where tra_id = ?', id, callback);
+}
+Resumos.prototype.editar = function (id, callback) {
+    this._connection.query("update trabalho set trab_inf_post = 1 where usu_id = ? ", id, callback);
+}
+Resumos.prototype.listaResumosPendentes = function (callback) {
+    this._connection.query('select * from trabalho where tra_verifica = 0', callback);
+};
+Resumos.prototype.listaCategorias = function (callback) {
+    this._connection.query('select * from categoria', callback);
+}
+Resumos.prototype.resumosLivroCategoria = function (id, callback) {
+    this._connection.query('select t.*, l.* from trabalho t inner join livro l on t.tra_id=l.tra_id where tra_cat_id = ?', id, callback);
+}
+Resumos.prototype.resumosAvaliacao = function (info, callback) {
+    this._connection.query('insert into estrela set ?', info, callback);
+}
+Resumos.prototype.getAvaliacao = function (id, callback) {
+    this._connection.query('select e.est_valor from estrela e where est_tra_id = ?', id, callback);
+}
+Resumos.prototype.newComent = function (usu_id, tra_id, comentario, callback) {
+    this._connection.query('insert into comentario (comnt_usu_id,comnt_tra_id,comnt_comentario) VALUES (?,?,?)', [usu_id, tra_id, comentario], callback);
+}
+Resumos.prototype.getComent = function (comnt_trab_id, callback) {
+    this._connection.query('select c.*,u.usu_nome,u.usu_sobrenome,u.usu_img_perfil from comentario c inner join trabalho t on c.comnt_tra_id=t.tra_id inner join usuario u on c.comnt_usu_id=u.usu_id where comnt_tra_id = ?', comnt_trab_id, callback);
+}
+
+
+module.exports = function () {
+    return Resumos;
+}
 
