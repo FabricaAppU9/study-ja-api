159319a176e244a22fc0422c1256568a9fe44dd0
Vinicius <Vinicius_k2013@hotmail.com>
2018-11-24 10:31:54 -0200

    input dynamic values to querie of new resumes

--- persistencia/ResumosDao.js
+++ persistencia/ResumosDao.js
@@ -4,19 +4,19 @@ function Resumos(connection) {
 
 Resumos.prototype.salvaResumoLivro = function (resumo, callback) {
     this._connection.query("START TRANSACTION;" +
-        "insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_texto,tra_dt_criacao,tra_dt_atualizacao,tra_visualizacoes)" +
-        "VALUES (1,2,testes,texto teste para inser com transaction, now(),now(),12);" +
+        "insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_texto,tra_dt_criacao,tra_visualizacoes)" +
+        "VALUES (?,?,?,?,now(),?);" +
         "insert into livro (tra_id,liv_nome,liv_editora,liv_volume,liv_ano)" +
-        "VALUES(LAST_INSERT_ID(),TESTE,testeeditora,volume 2,liv_ano);" +
-        "COMMIT;", resumo, callback);
+        "VALUES(LAST_INSERT_ID(),?,?,?);" +
+        "COMMIT;", [resumo[0],resumo[1],resumo[2],resumo[3],resumo[5],resumo[6],resumo[7],resumo[8],resumo[9]], callback);
 }
 Resumos.prototype.salvaResumoArtigo = function (resumo, callback) {
     this._connection.query("START TRANSACTION;" +
-        "insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_tipo,tra_texto,tra_dt_criacao,tra_dt_atualizacao,tra_visualizacoes)" +
-        "VALUES (1,2,testes,texto teste para inser com transaction, now(),now(),12);" +
+        "insert into trabalho (tra_usu_id,tra_cat_id,tra_descricao,tra_texto,tra_dt_criacao,tra_visualizacoes)" +
+        "VALUES (?,?,?,?,now(),?);" +
         "insert into artigo(tra_id,art_titulo)" +
-        "VALUES (LAST_INSERT_ID(),teste dois);" +
-        "COMMIT;", resumo, callback);
+        "VALUES (LAST_INSERT_ID(),?);" +
+        "COMMIT;", [resumo[0],resumo[1],resumo[2],resumo[3],resumo[5],resumo[6],resumo[7]], callback);
 }
 Resumos.prototype.listaResumosLivro = function (callback) {
     this._connection.query('select t.*,l.*,u.usu_nome from trabalho t inner join livro l on t.tra_id = l.tra_id inner join usuario u on t.tra_usu_id=u.usu_id', callback);
