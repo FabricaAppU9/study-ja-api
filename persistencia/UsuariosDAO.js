6ef46924c587da257af8433b6c5df0b354decff4
RafaelMarinho96 <rafael.marinho07@gmail.com>
2019-01-28 22:40:52 -0200

    Add field usu_descricao in /usuario/:id query

--- persistencia/UsuariosDAO.js
+++ persistencia/UsuariosDAO.js
@@ -18,7 +18,7 @@ Usuarios.prototype.deletar = function (id, callback) {
     this._connection.query("UPDATE usuario SET usu_enable = false where usu_id = ? ", id, callback);
 }
 Usuarios.prototype.getUserInfo = function (id, callback) {
-    this._connection.query("select usu_id, usu_nome, usu_sobrenome, usu_sexo, usu_dat_nascimento,usu_email,usu_cpf,usu_img_perfil from usuario where usu_id=?", id, callback);
+    this._connection.query("select usu_id, usu_nome, usu_sobrenome, usu_sexo, usu_dat_nascimento,usu_email,usu_cpf,usu_img_perfil, usu_descricao from usuario where usu_id=?", id, callback);
 }
 Usuarios.prototype.hasEmail = function (email, callback) {
     this._connection.query("select usu_id from usuario where usu_email = ?", email, callback);
